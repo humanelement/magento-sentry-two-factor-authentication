0c54c8a404d48291f8e11c180adab9f0127cf795
Seth Daugherty <sdaugherty@human-element.com>
2017-07-13 20:05:23 -0400

    add phpgangsta/googleauthenticator composer package

--- Model/Validate/Google.php
+++ Model/Validate/Google.php
@@ -1,29 +1,66 @@
 <?php
-/*
- * Author   : Aric Watson
- *            Nexcess.net - https://www.nexcess.net
+/**
+ * Human Element Inc.
  *
- * License  : GPL  -- https://www.gnu.org/copyleft/gpl.html
- *
- * For more information on Google Authenticator, please see -
- *   https://github.com/google/google-authenticator/wiki
- *
- * Some code based on previous work by Michael Kliewe/PHPGangsta
- *  https://github.com/PHPGangsta/GoogleAuthenticator
- *  http://www.phpgangsta.de/
+ * @package HE_TwoFactorAuth
+ * @copyright Copyright (c) 2017 Human Element Inc. (https://www.human-element.com)
+ * @author Aric Watson <https://www.nexcess.net>
+ * @link https://github.com/PHPGangsta/GoogleAuthenticator Some code based on previous work by Michael Kliewe/PHPGangsta
+ * @link http://www.phpgangsta.de/ Some code based on previous work by Michael Kliewe/PHPGangsta
+ * @link https://github.com/google/google-authenticator/wiki For more information on Google Authenticator
+ * @license GPL
+ * @license https://www.gnu.org/copyleft/gpl.html
  */
+
+namespace HE\TwoFactorAuth\Model\Validate;
 
 require_once(Mage::getBaseDir('lib') . DS . 'GoogleAuthenticator' . DS . 'PHPGangsta' . DS . 'GoogleAuthenticator.php');
 
-class HE_TwoFactorAuth_Model_Validate_Google extends HE_TwoFactorAuth_Model_Validate
+class Google extends \HE\TwoFactorAuth\Model\Validate
 {
 
-    public function __construct()
+    /**
+     * @var \HE\TwoFactorAuth\Helper\Data
+     */
+    protected $twoFactorAuthHelper;
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var \Magento\User\Model\UserFactory
+     */
+    protected $userUserFactory;
+
+    /**
+     * @var \Magento\Backend\Model\Auth\Session
+     */
+    protected $backendAuthSession;
+
+    /**
+     * @var 
+     */
+    protected $;
+
+    public function __construct(
+        \HE\TwoFactorAuth\Helper\Data $twoFactorAuthHelper,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\User\Model\UserFactory $userUserFactory,
+        \Magento\Backend\Model\Auth\Session $backendAuthSession,
+         $
+    )
     {
-        $this->_shouldLog = Mage::helper('he_twofactorauth')->shouldLog();
+        $this-> = $;
+        $this->twoFactorAuthHelper = $twoFactorAuthHelper;
+        $this->logger = $logger;
+        $this->userUserFactory = $userUserFactory;
+        $this->backendAuthSession = $backendAuthSession;
+        $this->_shouldLog = $this->twoFactorAuthHelper->shouldLog();
     }
 
-    /*
+    /**
      * HOTP - counter based
      * TOTP - time based
      */
@@ -31,33 +68,34 @@ class HE_TwoFactorAuth_Model_Validate_Google extends HE_TwoFactorAuth_Model_Vali
     {
         $token = $this->setUser($username, $tokentype);
         if ($this->_shouldLog) {
-            Mage::log("token = " . var_export($token, true));
+            $this->logger->debug("token = " . var_export($token, true));
         }
 
-        $user = Mage::getModel('admin/user')->loadByUsername($username);
+        $user = $this->userUserFactory->create()->loadByUsername($username);
         $user->setTwofactorauthToken($token);
         //$user->save(); //password gets messed up after saving?!
     }
 
-
+    /**
+     * @return bool
+     */
     public function isValid()
     {
         return true;
     }
 
-    /* 
+    /**
      * generates and returns a new shared secret
      */
     public function generateSecret()
     {
-        $ga = new PHPGangsta_GoogleAuthenticator();
+        $ga = $this->->create();
         $secret = $ga->createSecret();
 
         return $secret;
     }
 
-
-    /* 
+    /**
      * generates and returns QR code URL from google
      */
     public function generateQRCodeUrl($secret, $username)
@@ -66,56 +104,53 @@ class HE_TwoFactorAuth_Model_Validate_Google extends HE_TwoFactorAuth_Model_Vali
             return;
         }
 
-        $ga = new PHPGangsta_GoogleAuthenticator();
+        $ga = $this->->create();
         $url = $ga->getQRCodeGoogleUrl($username, $secret);
 
         return $url;
     }
 
-
-    /*
+    /**
      * verifies the code using TOTP
      */
-
     public function validateCode($code)
     {
         if (empty($code)) {
             return;
         }
-        Mage::log("Google - validateCode: " . $code, 0, "two_factor_auth.log");
+        $this->logger->log(\Monolog\Logger::EMERGENCY, "Google - validateCode: ".$code);
 
         // get user's shared secret
-        $user = Mage::getSingleton('admin/session')->getUser();
-        $admin_user = Mage::getModel('admin/user')->load($user->getId());
+        $user = $this->backendAuthSession->getUser();
+        $admin_user = $this->userUserFactory->create()->load($user->getId());
 
-        $ga = new PHPGangsta_GoogleAuthenticator();
+        $ga = $this->->create();
         $secret = Mage::helper('core')->decrypt($admin_user->getTwofactorGoogleSecret());
 
         return $ga->verifyCode($secret, $code, 1);
     }
 
-
-    /*
+    /**
      * abstract function in GoogleAuthenticator, needs to be defined here TODO
      */
     function getDataBad($username, $index = null) // this was causing problems, not sure why...
     {
-        $user = Mage::getModel('admin/user')->loadByUsername($username);
+        $user = $this->userUserFactory->create()->loadByUsername($username);
 
         return $user->getTwofactorauthToken() == null ? false : $user->getTwofactorauthToken();
     }
 
-    /*
+    /**
      * abstract function in GoogleAuthenticator, needs to be defined here
      */
     function putData($username, $data)
     {
-        $user = Mage::getModel('admin/user')->loadByUsername($username);
+        $user = $this->userUserFactory->create()->loadByUsername($username);
         $user->setTwofactorauthToken("test");
         $user->save();
     }
 
-    /*
+    /**
      * abstract function in GoogleAuthenticator, needs to be defined here
      */
     function getUsers()
